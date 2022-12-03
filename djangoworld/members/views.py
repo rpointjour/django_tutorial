08b35d29146aa2ed0f0c7e1c535782d801bff2c0
rjpking92 <roody.jerry@yahoo.com>
2022-12-03 17:42:16 -0500

    added http resp to view

--- djangoworld/members/views.py
+++ djangoworld/members/views.py
@@ -1,3 +1,8 @@
+# Add http response for views
+# Views are python functions that take http requests and returns an http response like in HTML docs
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+    return HttpResponse("Hello world!")
