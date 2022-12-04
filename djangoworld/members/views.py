3d4b4f835f6b08d2fc543e18a9d3bd8b9f904c5b
rjpking92 <roody.jerry@yahoo.com>
2022-12-04 15:10:45 -0500

    added index template

--- djangoworld/members/views.py
+++ djangoworld/members/views.py
@@ -2,13 +2,25 @@
 # Add http response for views
 # Add templates for response
 # template = loader.get_template('myfirst.html')
+"""
+For adding records:
+
+  mymembers = Members.objects.all().values()
+  output = ""
+  for x in mymembers:
+    output += x["firstname"]
+  return HttpResponse(output)
+  """
 from django.http import HttpResponse
 from django.template import loader
 from .models import Members
 
 def index(request):
   mymembers = Members.objects.all().values()
-  output = ""
-  for x in mymembers:
-    output += x["firstname"]
-  return HttpResponse(output)
+  template = loader.get_template('index.html')
+  context = {
+    'mymembers': mymembers,
+  }
+  return HttpResponse(template.render(context, request))
+  
+ 
