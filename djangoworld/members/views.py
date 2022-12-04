1188fd8d4887632c4d6e293117d8a8b472b7757c
rjpking92 <roody.jerry@yahoo.com>
2022-12-04 02:23:43 -0500

    added sqlite for dbshell

--- djangoworld/members/views.py
+++ djangoworld/members/views.py
@@ -1,9 +1,14 @@
 # Views are python functions that take http requests and returns an http response like in HTML docs
 # Add http response for views
 # Add templates for response
+# template = loader.get_template('myfirst.html')
 from django.http import HttpResponse
 from django.template import loader
+from .models import Members
 
 def index(request):
-  template = loader.get_template('myfirst.html')
-  return HttpResponse(template.render())
+  mymembers = Members.objects.all().values()
+  output = ""
+  for x in mymembers:
+    output += x["firstname"]
+  return HttpResponse(output)
