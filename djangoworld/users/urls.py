da267d2b8dd76f8c75d4f7a7b1357ea009a90c37
rjpking92 <roody.jerry@yahoo.com>
2022-12-05 02:17:31 -0500

    added variables template

--- djangoworld/users/urls.py
+++ djangoworld/users/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('variables/', views.variables, name='variables'),
 ]
--- djangoworld/users/views.py
+++ djangoworld/users/views.py
@@ -1,7 +1,19 @@
 from django.http import HttpResponse
 from django.template import loader
 
-# Create your views here.
+# Index View: Users Template
 def index(request):
     template = loader.get_template('users.html')
     return HttpResponse(template.render())
+
+# Variables View: Variables Template
+def variables(request):
+    template = loader.get_template('variables.html')
+    context = {     # Create variable(s) using context object to render to template
+        'user': 'new user',
+        'car' : 'Ford Mustang GT',
+        'cost' : '37000',
+        'yes' : 'true',
+        'temperature' : '25.4'
+    }
+    return HttpResponse(template.render(context, request))
