15072765ec3acf55245541869c781bb2d487d2b4
rjpking92 <roody.jerry@yahoo.com>
2022-12-05 15:47:45 -0500

    added users model

--- djangoworld/users/models.py
+++ djangoworld/users/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Users(models.Model):      # CREATE TABLE users_users ( col1, col2, col3, ...)
+    username = models.CharField(max_length=255)     # username varchar(255) 
+    email = models.CharField(max_length=255)        # email varchar(255) 
+    password = models.CharField(max_length=255)     # password varchar(255) 
