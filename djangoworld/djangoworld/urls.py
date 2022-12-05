0cf9c8bc7e5f71c57194013ef45f2498733bca8c
rjpking92 <roody.jerry@yahoo.com>
2022-12-04 23:26:47 -0500

    added users template

--- djangoworld/djangoworld/urls.py
+++ djangoworld/djangoworld/urls.py
@@ -20,5 +20,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('members/', include('members.urls')),
+    path('users/', include('users.urls')),
     path('admin/', admin.site.urls),
 ]
